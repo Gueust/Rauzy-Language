47fd5ea83f94c96a1c6c71d5c236a7e97d08e3a3
Gueust <jeanbaptiste.lespiau@gmail.com>
2013-10-10 13:29:12 +0200

    Added the abstraction representation of Rauzy objects and relations

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import json
 from pprint import pprint
 
+
+class Object:
+  """Abstract Rauzy object"""
+  def __init__(self):
+    self.extends = None
+    self.objects = {}
+    self.relations = {}
+    self.properties = {}
+
+  def add_object(name, obj):
+    self.objects[name] = obj
+
+  def add_relation(name, relation):
+    self.relations[name] = relation
+
+  def add_property(key, value):
+    self.properties[key] = value
+
+class Relation:
+  """Abstract Rauzy relation"""
+  def __init__(self):
+    self.extends = None
+    self.fromSet = {}
+    self.toSet = {}
+    self.directional = None
+    self.properties = {}
+
 def load_json(file, debug = False):
   """Open a file and parse it as json."""
   json_data = open(file)
