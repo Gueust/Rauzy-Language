82a1eea0d6d1c4832293114153bca09cb12fdd31
Gueust <jeanbaptiste.lespiau@gmail.com>
2013-10-10 23:19:19 +0200

    Beginning of the parsing of the json tree to build the Rauzy abtract objects

--- main.py
+++ main.py
@@ -46,6 +46,8 @@ def _directional(obj):
   else:
     return obj["directional"]
 
+def _library(obj):
+  return _get_value(obj, "library")
 
 
 class Object:
@@ -98,8 +100,82 @@ class Relation:
     self.directional = None
     self.properties = {}
 
+
+def parse_model(path):
+  obj = load_json(path)
+  """Parse a json object representing a model (i.e. a root object)"""
+  lib_file = _library(obj)
+  #TODO: define precisely the path of the library with respect to the path
+  if lib_file is None:
+    library = {}
+  else:
+    try:
+      location = open(lib_file)
+    except IOError as err:
+      raise IOError(format(err) + " \n The library path must be relative to the model file")
+  # build the library
+  #TODO: implement the loading of the library
+
+def _instanciate(class_name, library):
+  """Returns an instance of class_name present in library"""
+  if class_name not in library:
+    raise LookupError("The class " + class_name + " is not in the library")
+  #TODO: implement the instanciation
+  return Object()
+
+def parse_object(obj, library):
+  """Parse a json object and return the Rauzy object"""
+  # obj must be a dictionnary
+  nature = _nature(obj)
+  if nature != "object":
+    raise Exception("It is not an object")
+  #object = Object()
+  extends = _extends(obj)
+  if extends != None:
+    object = _instanciate(extends, library)
+  else:
+    object = Object()
+
+    contained_objs = _contained_objects(obj)
+    for name, contained_obj in contained_objs:
+      object.objects[name] = parse_object(contained_obj)
+
+    relations = _relations(obj)
+    for name, relation in relations:
+      object.relations[name] = parse_relation(relation)
+
+  properties = _properties(obj)
+  for key, value in properties:
+    # check that value is a string
+    object.properties[name] = value
+
+def parse_relation(rlt):
+  """Parse a json object representing a Rauzy relation and returns the
+  corresponding Rauzy relation"""
+  nature = _nature(rlt)
+  if nature != "relation":
+    raise Exception("It is not a relation")
+  extends = _extends(rlt)
+  if extends != None:
+    relation = _instanciate(extends)
+  else:
+    relation = Relation()
+
+    fromSet = _fromSet(rlt)
+    for name, contained_obj in fromSet:
+      relation.fromSet[name] = parse_object(contained_obj)
+
+    toSet = _toSet(obj)
+    for name, contained_obj in toSet:
+      relation.toSet[name] = parse_object(contained_obj)
+
+  properties = _properties(obj)
+  for key, value in properties:
+    #TODO: check that value is a string
+    relation.properties[name] = value
+
 def load_json(file, debug = False):
-  """Open a file and parse it as json."""
+  """Open a file and parse it as json"""
   json_data = open(file)
   data = json.load(json_data)
   if debug :
