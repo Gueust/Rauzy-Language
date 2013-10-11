62ac95058c84742df5aa5c3b45b5fdd908c1ae2f
Gueust <jeanbaptiste.lespiau@gmail.com>
2013-10-11 23:52:37 +0200

    Dividing into small modules

---
+++ library.py
@@ -0,0 +1,6 @@
+
+
+class Library:
+  def __init__(self):
+    self.dic_obj = {}
+    self.dic_rlt = {}
---
+++ model.py
@@ -0,0 +1,34 @@
+from library import Library
+from core import _library
+
+class Model:
+  def __init__(self, obj, lib):
+    self.lib = lib
+    self.obj = obj
+
+def parse_model(path):
+  """Parse a file as a json object representing a model (i.e. a root object)"""
+  lib_file = _library(obj)
+  #TODO: define precisely the path of the library with respect to path
+
+  obj_library = {}
+  rlt_library = {}
+
+  # Build the library
+  if lib_file is not None:
+    try:
+      location = open(lib_file)
+    except IOError as err:
+      raise IOError(format(err) + " \n The library path must be relative to the model file")
+  
+    json_lib = json.load(location)
+    # Implement the loading of the library: first the relations, then the objects
+    if "relations" in json_lib:
+      for relation_class, relation in json_lib["relations"]:
+        rlt_library[relation_class] = parse_relation(relation, lib=True)
+    if "objects" in json_lib:
+      for obj_class, object in json_lib["objects"]:
+        obj_library[obj_class] = parse_object(object, obj_library, rlt_library)
+  
+  obj = load_json(path)
+  return parse_object(obj, obj_library, rlt_library)
