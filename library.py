d94d2238a516fc1e07132ec30714da57d3bb5189
Gueust <jeanbaptiste.lespiau@gmail.com>
2013-10-12 01:26:46 +0200

    TODO added for the library

--- library.py
+++ library.py
@@ -1,6 +1,11 @@
-
+#TODO: 
+# - Build a dependency graph in addition to the dictionaries for objects.
+# - Ensure that this graph has no cycle
+# - List the objects in a correct order according to the decendency chain
+# - Do a loading and saving function
 
 class Library:
   def __init__(self):
     self.dic_obj = {}
     self.dic_rlt = {}
+
