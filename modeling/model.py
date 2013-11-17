5187e7045dd6a1cad639bc70a02027982e24aa40
Gueust <jeanbaptiste.lespiau@gmail.com>
2013-11-17 17:40:07 +0100

    The model module has been corrected and documented

--- modeling/model.py
+++ modeling/model.py
@@ -1,6 +1,24 @@
+r"""
+A Model contains both a library and an object description using that library.
+
+Loading a model from a file::
+
+  >>> from modeling.model import *
+  >>> loaded_model = Model.load('examples/car.model')
+
+Saving a model from a file::
+
+  >>> from modeling.model import *
+  >>> loaded_model = Model.load('examples/car.model')
+  >>> loaded_model.set_lib_path('car_lib.lib')
+  >>> loaded_model.set_obj_path('car_bis.model')
+  >>> loaded_model.save()
+
+"""
+
 import os, json, collections
-from . import core
-from .library import Library
+from .core import *
+from .library import *
 
 class Model:
 
@@ -9,6 +27,45 @@ class Model:
     self.lib_path = None
     self.obj = None
     self.model_name = None
+
+  def set_lib_path(self, lib_path: str):
+    """Set the name for the library file in order to be saved.
+
+    It is not needed to set the library path if the library is empty."""
+    self.lib_path = lib_path
+
+  def set_obj_path(self, obj_path: str):
+    """Set the name of the object file in order to be saved"""
+    self.model_name = obj_path
+
+  @staticmethod
+  def load(file):
+    """Parse a file as a json object representing a model (i.e. a root object)"""
+    json_data = open(file)
+    json_model = json.load(json_data)
+
+    resulting_model = Model()
+    rlt_library = resulting_model.lib.dic_rlt
+    obj_library = resulting_model.lib.dic_obj
+
+    # Build the library
+    lib_file = core._library(json_model)
+    if lib_file is not None:
+      try:
+        directory_path = os.path.dirname(file)
+        location = open(os.path.join(directory_path, lib_file))
+      except IOError as err:
+        raise IOError(format(err) + " \n The library path must be relative to the model file")
+
+      # We load the library using ordered dictionaries
+      json_lib = json.load(location)
+      resulting_model.lib .load(json_lib)
+
+    json_obj = load_json(file)
+    resulting_model.obj = Object.new(json_obj, resulting_model.lib)
+    resulting_model.model_name = os.path.basename(file)
+
+    return resulting_model
 
   def save(self, indentation=1):
     """Saves the model into an object file and a library file.
@@ -38,42 +95,6 @@ class Model:
       raise Exception("You are using a library without any name for it")
     if self.lib is not None:
       self.lib.save(os.path.join(os.path.dirname(self.model_name), self.lib_path))
-
-def load(file):
-  """Parse a file as a json object representing a model (i.e. a root object)"""
-  json_data = open(file)
-  json_model = json.load(json_data)
-
-  resulting_model = Model()
-  rlt_library = resulting_model.lib.dic_rlt
-  obj_library = resulting_model.lib.dic_obj
-
-  # Build the library
-  lib_file = _library(json_model)
-  if lib_file is not None:
-    try:
-      directory_path = os.path.dirname(file)
-      location = open(os.path.join(directory_path, lib_file))
-      print("localisation of the library file " + location)
-    except IOError as err:
-      raise IOError(format(err) + " \n The library path must be relative to the model file")
-  
-    # We load the library using ordered dictionaries
-    json_lib = json.load(location, object_pairs_hook=collections.OrderedDict())
-    # Implement the loading of the library: first the relations, then the objects
-    #TODO : modify this part in order to load any library
-    if "relations" in json_lib:
-      for relation_class, relation in json_lib["relations"]:
-        rlt_library[relation_class] = parse_relation(relation, resulting_model.lib, is_lib=True)
-    if "objects" in json_lib:
-      for obj_class, object in json_lib["objects"]:
-        obj_library[obj_class] = parse_object(object, resulting_model.lib, is_lib=True)
-  
-  json_obj = load_json(path)
-  resulting_model.obj = parse_object(obj, resulting_model)
-  resulting_model.model_name = os.path.basename(file)
-
-  return resulting_model
 
 if __name__ == "__main__":
   print("Testing model module")
